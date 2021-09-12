ec3c25c2603348eb1e36d9dc2dd4e6bae6a980cc
四条眉毛 <fourfishxiake@gmail.com>
2021-09-12 19:42:02 +0800

    computed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const targetMap = new WeakMap()
 
 const track = (target, key) => {
-  let depsMap = targetMap.get(targetMap);
+  console.log('track', key)
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target);
   if (!depsMap)  {
     targetMap.set(target, depsMap = new Map())
   }
@@ -11,10 +13,11 @@ const track = (target, key) => {
     depsMap.set(key, dep = new Set())
   }
 
-  dep.add(effect)
+  dep.add(activeEffect)
 }
 
 const trigger = (target, key) => {
+  console.log('trigger', key)
   const depsMap = targetMap.get(target)
 
   if(!depsMap) {
@@ -25,22 +28,78 @@ const trigger = (target, key) => {
   if(!dep) {
     return
   }
-
   dep.forEach(effect => {
     effect()
   });
 }
 
-let product = { price: 2, quality: 2 }
-let total = 0
-let effect = () => {
-  total = product.price * product.quality
+const reactive = (target) => {
+  const handler = {
+    get(target, key, receiver) {
+      let result = Reflect.get(target, key, receiver)
+      track(target, key)
+      return result
+    },
+    set(target, key, value, receiver) {
+      let oldValue = target[key]
+      let result = Reflect.set(target, key, value, receiver)
+      if (result && oldValue !== value) {
+        trigger(target, key)
+      }
+      return result
+    }
+  }
+  return new Proxy(target, handler)
 }
 
-track(product, 'price')
-effect()
-console.log(total)
+const ref = (val) => {
+  const r = {
+    get value() {
+      track(r, 'value')
+      return val
+    },
 
+    set value(newVal) {
+      if (newVal === val) return
+      val = newVal
+      trigger(r, 'value')
+    }
+  }
+  return r
+}
+
+let activeEffect = null
+let effect = (fn) => {
+  activeEffect = fn
+  activeEffect()
+  activeEffect = null
+}
+
+const computed = (getter) => {
+  const res = ref()
+  effect(() => res.value = getter())
+  return res
+}
+
+let product = reactive({ price: 5, quantity: 2 })
+let salePrice = computed(() => {
+  return product.price * 0.9
+})
+
+let total = computed(() => {
+  return salePrice.value * product.quantity
+})
+
+
+console.log(
+  `Before updated quantity total (should be 9) = ${total.value} salePrice (should be 4.5) = ${salePrice.value}`
+)
+product.quantity = 3
+console.log(
+  `After updated quantity total (should be 13.5) = ${total.value} salePrice (should be 4.5) = ${salePrice.value}`
+)
 product.price = 10
-trigger(product, 'price')
-console.log(total)
+console.log(
+  `After updated price total (should be 27) = ${total.value} salePrice (should be 9) = ${salePrice.value}`
+)
+
